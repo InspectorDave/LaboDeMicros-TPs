d55dede643eb31f36aec850e5c4f2a91d84714cb
Eze M <ez.equidave@gmail.com>
2022-11-10 03:58:28 -0300

    fix: corrijo las interrupciones y el bouncing

--- TP2/ParteB/DisplaySubroutines.asm
+++ TP2/ParteB/DisplaySubroutines.asm
@@ -5,140 +5,220 @@
 
 cicleSegments: ;prendo cada led de a uno por 1 segundo
 	ldi aux, segA
-	out PORTB, aux
+	out PORTC, aux
 	call delay8Mcicles
+
 	ldi aux, segB
-	out PORTB, aux
+	out PORTC, aux
 	call delay8Mcicles
+
 	ldi aux, segC
-	out PORTB, aux
+	out PORTC, aux
 	call delay8Mcicles
+
 	ldi aux, segD
-	out PORTB, aux
+	out PORTC, aux
 	call delay8Mcicles
+
 	ldi aux, segE
-	out PORTB, aux
+	out PORTC, aux
 	call delay8Mcicles
+
 	ldi aux, segF
+	out PORTC, aux
+	call delay8Mcicles
+
+	clr aux
+	out PORTC, aux ;limpio el puerto 
+	ldi aux, segG  ;como esto sale por el puerto B, lo corro al pin correspondiente
 	out PORTB, aux
 	call delay8Mcicles
-	ldi aux, segG
-	out PORTB, aux
+
+	clr aux        
+	out PORTC, aux ;limpio el puerto C
+	ldi aux, segO 
+	out PORTB, aux 
 	call delay8Mcicles
-	ldi aux, segO
-	out PORTB, aux
-	call delay8Mcicles
+
+	clr aux
+	out PORTB, aux ;apago todos los LEDs
+	out PORTC, aux ;apago todos los LEDs
 	ret
 
 displayValue: 
-;Establece el PORTB con los bits correspondientes
+;Establece el PORTC y PORTB con los bits correspondientes
 ;al número que se quiere mostrar
 	display0:
 		cpi dispValue, 0x0
 		brne display1
-		ldi aux, disp0
+		ldi aux, disp0C
+		out PORTC, aux
+		ldi aux, disp0B
+		out PORTB, aux
 		rjmp ret0
 	display1:
 		cpi dispValue, 0x1
 		brne display2
-		ldi aux, disp1
+		ldi aux, disp1C
+		out PORTC, aux
+		ldi aux, disp1B
+		out PORTB, aux
 		rjmp ret0
 	display2:
 		cpi dispValue, 0x2
 		brne display3
-		ldi aux, disp2
+		ldi aux, disp2C
+		out PORTC, aux
+		ldi aux, disp2B
+		out PORTB, aux
 		rjmp ret0
 	display3:
 		cpi dispValue, 0x3
 		brne display4
-		ldi aux, disp3
+		ldi aux, disp3C
+		out PORTC, aux
+		ldi aux, disp3B
+		out PORTB, aux
 		rjmp ret0
 	display4:
 		cpi dispValue, 0x4
 		brne display5
-		ldi aux, disp4
+		ldi aux, disp4C
+		out PORTC, aux
+		ldi aux, disp4B
+		out PORTB, aux
 		rjmp ret0
 	display5:
 		cpi dispValue, 0x5
 		brne display6
-		ldi aux, disp5
+		ldi aux, disp5C
+		out PORTC, aux
+		ldi aux, disp5B
+		out PORTB, aux
 		rjmp ret0
 	display6:
 		cpi dispValue, 0x6
 		brne display7
-		ldi aux, disp6
+		ldi aux, disp6C
+		out PORTC, aux
+		ldi aux, disp6B
+		out PORTB, aux
 		rjmp ret0
 	display7:
 		cpi dispValue, 0x7
 		brne display8
-		ldi aux, disp7
+		ldi aux, disp7C
+		out PORTC, aux
+		ldi aux, disp7B
+		out PORTB, aux
 		rjmp ret0
 	display8:
 		cpi dispValue, 0x8
 		brne display9
-		ldi aux, disp8
+		ldi aux, disp8C
+		out PORTC, aux
+		ldi aux, disp8B
+		out PORTB, aux
 		rjmp ret0
 	display9:
 		cpi dispValue, 0x9
 		brne displayA
-		ldi aux, disp9
+		ldi aux, disp9C
+		out PORTC, aux
+		ldi aux, disp9B
+		out PORTB, aux
 		rjmp ret0
 	displayA:
 		cpi dispValue, 0xA
 		brne displayB
-		ldi eepromAddress, 0x00
-		call eepromRead
+		ldi eepromAddress,0x0
+		call eepromRead 
+		out PORTC, aux
+		call eepromRead 
+		out PORTB, aux
 		rjmp ret0
 	displayB:
 		cpi dispValue, 0xB
 		brne displayC
-		ldi eepromAddress, 0x01
-		call eepromRead
+		ldi eepromAddress,0x2
+		call eepromRead 
+		out PORTC, aux
+		call eepromRead 
+		out PORTB, aux
 		rjmp ret0
 	displayC:
 		cpi dispValue, 0xC
 		brne displayD
-		ldi eepromAddress, 0x02
-		call eepromRead
+		ldi eepromAddress,0x4
+		call eepromRead 
+		out PORTC, aux
+		call eepromRead 
+		out PORTB, aux
 		rjmp ret0
 	displayD:
 		cpi dispValue, 0xD
 		brne displayE
-		ldi eepromAddress, 0x03
-		call eepromRead
+		ldi eepromAddress,0x6
+		call eepromRead 
+		out PORTC, aux
+		call eepromRead 
+		out PORTB, aux
 		rjmp ret0
 	displayE:
 		cpi dispValue, 0xE
 		brne displayF
-		ldi eepromAddress, 0x04
-		call eepromRead
+		ldi eepromAddress,0x8
+		call eepromRead 
+		out PORTC, aux
+		call eepromRead 
+		out PORTB, aux
 		rjmp ret0
 	displayF:
 		cpi dispValue, 0xF
 		brne displayError
-		ldi eepromAddress, 0x05
-		call eepromRead
+		ldi eepromAddress,0xA
+		call eepromRead 
+		out PORTC, aux
+		call eepromRead 
+		out PORTB, aux
 		rjmp ret0
 	displayError:
 		ldi dispValue, 0
-		ldi aux, disp0
+		rjmp display0
 	ret0:
-		out PORTB, aux
 		ldi eepromAddress,lastValueAddress
 		call eepromWrite
 		ret
 
 writeLettersInEEprom:
-	ldi aux, dispA
+	ldi eepromAddress,0x0
+	ldi aux, dispAC
 	call eepromWrite
-	ldi aux, dispB
+	ldi aux, dispAB
 	call eepromWrite
-	ldi aux, dispC
+
+	ldi aux, dispBC
 	call eepromWrite
-	ldi aux, dispD
+	ldi aux, dispBB
 	call eepromWrite
-	ldi aux, dispE
+
+	ldi aux, dispCC
 	call eepromWrite
-	ldi aux, dispF
+	ldi aux, dispCB
+	call eepromWrite
+
+	ldi aux, dispDC
+	call eepromWrite
+	ldi aux, dispDB
+	call eepromWrite
+
+	ldi aux, dispEC
+	call eepromWrite
+	ldi aux, dispEB
+	call eepromWrite
+
+	ldi aux, dispFC
+	call eepromWrite
+	ldi aux, dispFB
 	call eepromWrite
 	ret
--- TP2/ParteB/main.asm
+++ TP2/ParteB/main.asm
@@ -8,56 +8,60 @@
 
 .def aux = r16
 .def eepromAddress = r21
-.def dispValue = r17 ;The value that will be displayed
-.equ lastValueAddress = 0x06 ;EEprom's address where the last value will be saved
+.def dispValue = r17 ;el valor que se mostrará
+.equ lastValueAddress = 0x0C ;EEprom's address where the last value will be saved
 
 .cseg
 .org 0x0000
-	rjmp	start
+	rjmp start
 .org INT0addr
-	cli
 	rjmp handlerIntExt0
 .org INT1addr
-	cli
 	rjmp handlerIntExt1
 
 .org INT_VECTORS_SIZE
 
 start:
+	cli
 	rcall configure_ports
 	rcall configure_int0
 	rcall configure_int1
 	rcall enable_int0
 	rcall enable_int1
-	sei
 
 	call writeLettersInEEprom
-
 	call cicleSegments
 
-	ldi eepromAddress, lastValueAddress
-	call eepromRead
+;	ldi dispValue,0
 	call displayValue
 
+	sei
+
 	main_loop:
+		sleep
 		rjmp  main_loop
 
 configure_ports:
 	ldi aux, 0xFF
-	out DDRB,aux
-	cbi DDRD,DDD2
-	cbi DDRD,DDD3
+	out DDRB, aux  ;configuro puerto B como out
+	out DDRC, aux  ;configuro puerto C como out
+	cbi DDRD, DDD2 ;configuro los pines del puerto D como in
+	cbi DDRD, DDD3
+	sbi PORTD, DDD2  ;prendo las resistencias de pull-up de los pines del pureto D
+	sbi PORTD, DDD3 
 	ret
 
-configure_int0: ;por flanco ascendente
+configure_int0: ;por flanco descendente
 	lds  aux, EICRA
-	ori  aux, (1 << ISC00) | (1 << ISC01)
+	ori  aux, (1 << ISC01)
+	andi aux, ~( (1 << ISC00) )
 	sts  EICRA, aux
 	ret
 
-configure_int1: ;por flanco ascendente
+configure_int1: ;por flanco descendente
 	lds  aux, EICRA
-	ori  aux, (1 << ISC10) | (1 << ISC11)
+	ori  aux, (1 << ISC11)
+	andi aux, ~( (1 << ISC10) )
 	sts  EICRA, aux
 	ret
 
@@ -74,23 +78,35 @@ enable_int1:
 	ret
 
 handlerIntExt0:
-	cpi dispValue, 0xF
+	call delay16kcicles ;compruebo que no haya sido un error
+	in aux, PIND
+	andi aux, 0b00000100
+	cpi aux, 0b00000000
+	brne reti1
+	
+	cpi dispValue, 0xF ;compruebo que pueda seguir incrementando
 	breq reti0
+	
 	inc dispValue
 	call displayValue
-	sei
 	reti0: reti
 
 handlerIntExt1:
-	cpi dispValue, 0x0
+	call delay16kcicles ;compruebo que no haya sido un error
+	in aux, PIND
+	andi aux, 0b00001000
+	cpi aux, 0b00000000
+	brne reti1
+
+	cpi dispValue, 0x0 ;compruebo que pueda seguir incrementando
 	breq reti1
+
 	dec dispValue
 	call displayValue
-	sei
 	reti1: reti
 
-delay8Mcicles:
-	ldi r18, 1
+delay8Mcicles: ;0,5s a 16MHz
+	ldi r18, 41
 	ldi r19, 150
 	ldi r20, 125
 	L1:
@@ -100,6 +116,21 @@ delay8Mcicles:
 		brne L1
 		dec r18
 		brne L1
+	nop
+	ret
+
+delay16kcicles: ;1ms a 16MHz
+	ldi r18, 9
+	ldi r19, 30
+	ldi r20, 226
+	L2:
+		dec r20
+		brne L2
+		dec r19
+		brne L2
+		dec r18
+		brne L2
+	nop
 	nop
 	ret
 
