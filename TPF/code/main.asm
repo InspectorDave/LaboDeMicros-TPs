d30590b21fa70adc969e9f43e3ab1aa2065f5a56
Eze M <ez.equidave@gmail.com>
2022-12-03 05:55:23 -0300

    Agrego matcheo del signo morse con su char ASCII

--- TPF/code/main.asm
+++ TPF/code/main.asm
@@ -8,10 +8,11 @@
 .def aux = r16
 .def aux2 = r17
 .def transmitChar = r18
+.def counter = r19 
 
 .dseg
 .org SRAM_START
-	readSign:	.byte	6 ;Acá se guarda el signo leído, con un \0 al final
+	readSign:	.byte	7 ;Acá se guarda el signo leído, con un \0 al final
 
 .cseg
 .org 0x0000
@@ -21,13 +22,31 @@
 
 start:	
 	call configurePorts
+	
+	ldi	xh, high(readSign)
+	ldi	xl, low (readSign)
 
-
-
+	ldi aux, '.'
+	st x+, aux
+	ldi aux, '.'
+	st x+, aux
+	ldi aux, '-'
+	st x+, aux
+	ldi aux, '-'
+	st x+, aux
+	ldi aux, '.'
+	st x+, aux
+	ldi aux, '.'
+	st x+, aux
+	ldi aux, 0
+	st x, aux
+	
+mainLoop:
 	call identifyChar
+
 	call transmitUsart
 
-	rjmp start
+	rjmp mainLoop
 
 configurePorts:
 	cbi DDRD, DDD0 ;configuro el pin RXD como entrada
@@ -38,88 +57,84 @@ configurePorts:
 .include "USART.asm"
 
 identifyChar:
-		ldi	xh, high(readSign)
-		ldi	xl, low (readSign)
-		ldi	zh, high(MORSE_A)
-		ldi	zl, low (MORSE_A)
-	loopA:
-		ld aux, x+
-		ld aux2, z+
-		cp aux, aux2
-		brne charB
-		cpi aux, '0'
-		breq loopA
-		ldi transmitChar, 'A'
-		ret
-	charB:
-		ldi	xh, high(readSign)
-		ldi	xl, low (readSign)
-		ldi	zh, high(MORSE_B)
-		ldi	zl, low (MORSE_B)
-	loopB:
-		ld aux, x+
-		ld aux2, z+
-		cp aux, aux2
-		brne charC
-		cpi aux, '0'
-		breq loopB
-		ldi transmitChar, 'B'
-		ret
-	charC:
-		ldi	xh, high(readSign)
-		ldi	xl, low (readSign)
-		ldi	zh, high(MORSE_C)
-		ldi	zl, low (MORSE_C)
-	loopC:
-		ld aux, x+
-		ld aux2, z+
-		cp aux, aux2
-		brne charD
-		cpi aux, '0'
-		breq loopC
-		ldi transmitChar, 'B'
-		ret
-	charD:
-		nop
+	ldi	xh, high(readSign)
+	ldi	xl, low (readSign)
+	ldi	zh, high(morseChars*2)
+	ldi	zl, low (morseChars*2)
+	ldi counter, 0
 
-MORSE_A: .db ".-", 0
-MORSE_B: .db "-...", 0
-MORSE_C: .db "-.-.", 0
-MORSE_D: .db "-..", 0
-MORSE_E: .db ".", 0
-MORSE_F: .db "..-.", 0
-MORSE_G: .db "--.", 0
-MORSE_H: .db "....", 0
-MORSE_I: .db "..", 0
-MORSE_J: .db ".---", 0
-MORSE_K: .db "-.-", 0
-MORSE_L: .db ".-..", 0
-MORSE_M: .db "--", 0
-MORSE_N: .db "-.", 0
-MORSE_O: .db "---", 0
-MORSE_P: .db ".--.", 0 
-MORSE_Q: .db "--.-", 0 
-MORSE_R: .db ".-.", 0 
-MORSE_S: .db "...", 0 
-MORSE_T: .db "-", 0 
-MORSE_U: .db "..-", 0 
-MORSE_V: .db "...-", 0 
-MORSE_W: .db ".--", 0 
-MORSE_X: .db "-..-", 0 
-MORSE_Y: .db "-.--", 0 
-MORSE_Z: .db "--..", 0 
-MORSE_0: .db "-----", 0
-MORSE_1: .db ".----", 0 
-MORSE_2: .db "..---", 0 
-MORSE_3: .db "...--", 0 
-MORSE_4: .db "....-", 0 
-MORSE_5: .db ".....", 0 
-MORSE_6: .db "-....", 0 
-MORSE_7: .db "--...", 0 
-MORSE_8: .db "---..", 0 
-MORSE_9: .db "----.", 0 
-MORSE_DOT: .db ".-.-.-", 0 
-MORSE_COMMA: .db "--..--", 0 
-MORSE_QMRK: .db "..--..", 0 
-MORSE_QUOTES: .db ".-..-.", 0 
-MORSE_SLASH: .db "-..-.", 0
+	loop1:
+		ld aux, x+
+		lpm aux2, z
+		cp aux, aux2
+		brne goToNextSign
+		lpm aux2, z+   ;Uso esto solo para incrementar z+
+		cpi aux, 0
+		breq matchSignWithChar
+		rjmp loop1
+
+
+
+goToNextSign:
+	lpm aux2, z+
+	cpi aux2, 0
+	brne goToNextSign
+
+	inc counter
+	ldi	xh, high(readSign)
+	ldi	xl, low (readSign)
+
+	rjmp loop1
+
+matchSignWithChar:
+	cpi counter, 26 ;Veo si es una de las letras
+	brlo itsLetter
+	cpi counter, 36 ;Veo si es uno de los números
+	brlo itsNumber
+	rjmp itsSign    ;No es ni letra ni número, debe ser signo de puntuación
+
+itsLetter:
+	ldi transmitChar, 65      ;La posición de la A en ASCII
+	add transmitChar, counter ;En base al valor del contador sé qué letra es
+	ret
+
+itsNumber:
+	ldi transmitChar, 22       ;La posición del 0 en ASCII (48) menos las 26 posiciones del contador que no son números
+	add transmitChar, counter  ;Dado a que ya resté 26, al sumar el contador sé qué número es
+	ret
+
+itsSign:
+	;No se pueden ordenar los signos en mi mapa Morse igual que en la tabla ASCII, hago if-else
+	cpi counter, 36
+	brne comma
+	ldi transmitChar, 46
+	ret
+	comma:
+	cpi counter, 37
+	brne questionMark
+	ldi transmitChar, 44	
+	ret
+	questionMark:
+	cpi counter, 38
+	brne quotes
+	ldi transmitChar, 63	
+	ret
+	quotes:
+	cpi counter, 39
+	brne backslash
+	ldi transmitChar, 34	
+	ret
+	backSlash:
+	ldi transmitChar, 92	
+	ret
+
+morseChars: .db ".-", 0, "-...", 0, "-.-.", 0, "-..", 0, ".", 0, "..-.", 0, "--.", 0, "....", 0, "..", 0,\
+	".---", 0, "-.-", 0, ".-..", 0, "--", 0, "-.", 0, "---", 0, ".--.", 0 , "--.-", 0, ".-.", 0, "...", 0,\
+	"-", 0, "..-", 0, "...-", 0, ".--", 0, "-..-", 0, "-.--", 0, "--..", 0, "-----", 0, ".----", 0,\
+	"..---", 0, "...--", 0, "....-", 0, ".....", 0, "-....", 0, "--...", 0, "---..", 0, "----.", 0,\
+	".-.-.-", 0, "--..--", 0, "..--..", 0, ".-..-.", 0, "-..-.", 0
+	; A B C D E F G H I
+	; J K L M N O P Q R S
+	; T U V W X Y Z 0 1
+	; 2 3 4 5 6 7 8 9 
+	; . , ? " \
